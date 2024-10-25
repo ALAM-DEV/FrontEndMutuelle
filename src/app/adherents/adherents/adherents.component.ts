80a52a50bec10109a26b8316cee059ead37ac73b
SimplePeoject <testinfo@gmail.com>
2024-10-25 19:33:52 +0200

    ajout compent adherents

---
+++ src/app/adherents/adherents/adherents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adherents',
+  templateUrl: './adherents.component.html',
+  styleUrls: ['./adherents.component.css']
+})
+export class AdherentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
